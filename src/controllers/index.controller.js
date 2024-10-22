f4b0d5bfc180f5da6ef3400cd695cecbd346e4e3
SeesonLau <seesonjohnlau@gmail.com>
2024-10-23 00:17:43 +0800

    Adjusted codelines

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 // Credentials are from https://developer.edamam.com/admin/applications
 const apiBaseUrl = 'https://api.edamam.com';
 const apiPathV2 = '/api/recipes/v2';
-
 const apiUrl = apiBaseUrl + apiPathV2;
 const apiId = '2bb5540e';
 const apiAppKey = 'ae5f39e076976feeede8020af1287163';
@@ -18,12 +17,10 @@ const apiAppKey = 'ae5f39e076976feeede8020af1287163';
  */
 const getRecipeList = async (request, res)=>{
     const type = 'public'; // This can be public, any, or user. Check API docs for more information.
-
     // Validate required query parameters
     if (!type || !apiId || !apiAppKey) {
         return res.status(400).json({ error: 'Missing required query parameters.' });
     }
-
     try {
         const response = await axios.get(apiUrl, {
             params: {
@@ -31,23 +28,18 @@ const getRecipeList = async (request, res)=>{
                 app_key: apiAppKey,
                 type: type,
                 q: request.query.ingredients,
-
             }
         });
-
         if (response.status === 200) {
             const hits = response.data.hits || [];
-
             if (hits.length === 0) {
                 // No data case (status is 200 but no hits)
                 return res.status(200).json({ message: 'No recipes found!' });
             }
-
             // Map the data if hits exist
             const recipesData = hits.map((hit, index) => {
                 const recipeData = hit.recipe;
                 console.log('BE data: ', recipeData)
-
                 return {
                     recipe_id: index + 1,
                     name: recipeData.label,
@@ -58,10 +50,9 @@ const getRecipeList = async (request, res)=>{
                     url: recipeData.url,
                     dish_type: recipeData.dishType,
                     cuisine_type: recipeData.cuisineType,
-                    calowee: Math.round(parseFloat(recipeData.calories))
+                    calorie: Math.round(parseFloat(recipeData.calories))
                 };
             });
-
             return res.json(recipesData);
         }
     } catch (error) {
@@ -70,7 +61,6 @@ const getRecipeList = async (request, res)=>{
         res.status(error.response ? error.response.status : 500).json({ error: 'Error fetching data from external API.' });
     }
 };
-
 module.exports = {
     getRecipeList,
 };
